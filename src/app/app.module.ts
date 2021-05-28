35f9f2aa407b52f0caec08e29007b22e95d01ea8
Chitranshu <chitranshus31@gmail.com>
2021-05-28 14:41:36 +0530

    New Component for dashboard added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ShareModule } from './shared/share.module';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardPageComponent } from '../app/dashboard/dashboard-page/dashboard-page.component';
+import { DashboardModule } from './dashboard/dashboard.module';
+
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'dashboard', component: DashboardPageComponent },
+];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ShareModule],
+  imports: [
+    BrowserModule,
+    DashboardModule,
+    ShareModule,
+    RouterModule.forRoot(routes),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
